f22032d834b7d0cb5049bba4e59ca878f284e6ef
Emmanuel Roberson <emmanuel.roberson3@gmail.com>
2020-02-02 14:51:52 -0600

    Final Commit

--- Assets/Scripts/Kyra/CollectBehaviour.cs
+++ Assets/Scripts/Kyra/CollectBehaviour.cs
@@ -1,16 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollectBehaviour : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    public GameEvent eyecollect1;
+    public GameEvent eyecollect2;
+
+    public int fakeDestroy = 0;
+
+    private float inputBuffer;
+
+
+    private void Start()
     {
-        if (other.gameObject.CompareTag("sphere"))
+        inputBuffer = 0;
+    }
+
+    private void Update()
+    {
+        if (fakeDestroy == 0)
         {
-            ItemBehaviour.theScore += 10;
-            Destroy(other.gameObject);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                fakeDestroy++;
+                eyecollect1.Raise();
+                inputBuffer = 0;
+            }
         }
 
+        if (fakeDestroy == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && inputBuffer >= 2)
+            {
+                fakeDestroy++;
+                eyecollect2.Raise();
+            }
+        }
+
+        if (fakeDestroy == 2)
+        {
+            if (Input.GetKey(KeyCode.Backspace))
+            {
+                StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+            }
+        }
+
+        inputBuffer += Time.deltaTime;
+    }
+    
+    public void LoadNextScene()
+    {
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        
+        yield return new WaitForSeconds(0);
     }
 }
