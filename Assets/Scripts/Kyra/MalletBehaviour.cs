22156eca154c2a3ba7084d42a794ad9e2a969b2d
KyraWooders <54289133+KyraWooders@users.noreply.github.com>
2020-02-02 13:21:37 -0600

    mallet behaviour

---
+++ Assets/Scripts/Kyra/MalletBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using RhythmTool;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MalletBehaviour : MonoBehaviour
+{
+    public RhythmEventProvider hh;
+
+    private void Start()
+    {
+        hh.Register<Beat>(OnBeat);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnBeat(Beat beat)
+    {
+        if (transform.rotation.x >= 0)
+        {
+            transform.SetPositionAndRotation(transform.position, new UnityEngine.Quaternion(90,0,0,0));
+        }
+        if (transform.rotation.x == 90)
+        {
+            transform.SetPositionAndRotation(transform.position, new UnityEngine.Quaternion(0, 0, 0, 0));
+        }
+
+    }
+
+}
